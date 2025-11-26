2882f4c8a045612a5ccfa7111235d614b8bdb490
abdurasulnematxonov <abdurasulnematxonov@gmail.com>
2025-11-26 10:34:17 +0000

    fixes

--- backend/pages/models.py
+++ backend/pages/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Club(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField()
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    members = models.ManyToManyField(User, related_name='clubs')
+    created_at = models.DateField()
+    updated_at = models.DateField()
+    logo = models.ImageField(upload_to='clubs')
+    banner = models.ImageField(upload_to='clubs')
+    event_manager = models.ForeignKey(User, on_delete=models.CASCADE)
+    events = models.ManyToManyField('Event', related_name='clubs')
+
+class Event(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField()
+    created_at = models.DateField()
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    club = models.ForeignKey()
